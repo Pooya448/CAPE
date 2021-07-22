d0c56c8c11da967d505539bafb8914cda3bd4c7f
Pooya Kabiri <pooya_kabiri@yahoo.com>
2021-07-23 04:04:20 +0430

    Add script

---
+++ download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
+read -p "Username:" username
+read -p "Password:" password
+username=$(urle $username)
+password=$(urle $password)
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=cape&sfile=CAPE_dataset.zip.001' -O 'CAPE_dataset.zip.001'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=cape&sfile=CAPE_dataset.zip.002' -O 'CAPE_dataset.zip.002'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=cape&sfile=CAPE_dataset.zip.002' -O 'CAPE_dataset.zip.003'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=cape&sfile=CAPE_dataset.zip.002' -O 'CAPE_dataset.zip.004'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=cape&sfile=CAPE_dataset.zip.002' -O 'CAPE_dataset.zip.005'
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=cape&sfile=CAPE_dataset.zip.002' -O 'CAPE_dataset.zip.006'
