8002c8610396559c3c7611666260d021b43b0732
Pooya Kabiri <pooya_kabiri@yahoo.com>
2021-07-23 04:06:26 +0430

    Add script

--- download.sh
+++ download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ..
+
 urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
 read -p "Username:" username
 read -p "Password:" password
